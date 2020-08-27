542a85ce4320a261cf70a590474764c81cae8bd9
Akhator Itua <iakhator@gmail.com>
2020-08-27 17:22:31 +0100

    add enviroment variable

--- src/config/aws.config.js
+++ src/config/aws.config.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk');
 
-AWS.config.update({accessKeyId: process.accessKeyId, secretAccessKey: process.env.secretAccessKey, region: 'us-east-2' });
+AWS.config.update({accessKeyId: process.env.accessKeyId, secretAccessKey: process.env.secretAccessKey, region: 'us-east-2' });
 
 const dynamodb = new AWS.DynamoDB();
 const docClient = new AWS.DynamoDB.DocumentClient();
